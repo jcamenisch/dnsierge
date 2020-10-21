cfc1654ce0b2c34c8952845aa0df6113ff3bb406
Jonathan Camenisch <jcamenisch@agari.com>
2020-10-21 14:10:53 -0400

    Correct lookup to just use NS queries

--- lib/nameservers.ex
+++ lib/nameservers.ex
@@ -7,27 +7,21 @@ defmodule Nameservers do
     Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
-  def from_dig(type, domain, server) do
-    type = String.upcase(type)
-
-    cmd_lines("dig", ["@#{server}", type, domain])
-    |> Stream.map(&Regex.named_captures(~r/#{type}\t(?<host>\S+)/, &1)["host"])
+  def from_dig(domain, server) do
+    run_cmd(["dig", "@#{server}", "ns", Regex.replace(~r/\\.*$/, domain, ".")])
+    |> String.split("\n")
+    |> Stream.map(&Regex.named_captures(~r/NS\t(?<host>\S+)/, &1)["host"])
     |> Enum.filter(& &1)
   end
 
-  def from_ns(domain, server) do
-    from_dig("NS", domain, server)
+  def run_cmd(cmd) do
+    IO.puts(Enum.join(cmd, " "))
+    {resp, 0} = System.cmd(hd(cmd), tl(cmd))
+    resp
   end
 
-  def from_soa(domain, server) do
-    from_dig("SOA", domain, server)
-  end
-
-  def from_whois(domain, _) do
-    cmd_lines("whois", [domain])
-    |> Stream.map(&Regex.named_captures(~r/^nserver\S*\s*(?<host>\S+)/, &1)["host"])
-    |> Stream.filter(& &1)
-    |> Enum.map(&String.downcase/1)
+  def get_for_segments([""]) do
+    @root_servers
   end
 
   def get_for_segments([]) do
@@ -41,9 +35,8 @@ defmodule Nameservers do
       cached_value
     else
       server = Enum.random(get_for_segments(tl(segments)))
-      result = [&from_ns/2, &from_soa/2, &from_whois/2]
-               |> Stream.map(fn f -> f.("#{Enum.join(segments, ".")}", server) end)
-               |> Enum.find([], fn l -> !Enum.empty?(l) end)
+      result = from_dig(Enum.join(segments, "."), server)
+
       Agent.update(__MODULE__, &(Map.put(&1, segments, result)))
 
       result
@@ -56,11 +49,5 @@ defmodule Nameservers do
 
   def get(fqdn) do
     fqdn |> String.split(".") |> get_for_segments()
-  end
-
-  defp cmd_lines(cmd, args) do
-    IO.puts(Enum.join([cmd] ++ args, " "))
-    {resp, 0} = System.cmd(cmd, args)
-    String.split(resp, "\n")
   end
 end
